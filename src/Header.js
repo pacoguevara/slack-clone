8714360b936ed0aa62d8d133d2b17da0b626d213
Paco Guevara <pacoguevaraa@gmail.com>
2020-08-20 18:26:46 -0700

    add header files

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        {/* avatar for logged in user */}
+        {/* Time icon */}
+      </div>
+      <div className="header__search">
+        {/* search icon */}
+        {/* input */}
+      </div>
+      <div className="header__right">{/* help icon */}</div>
+    </div>
+  );
+}
+
+export default Header;
