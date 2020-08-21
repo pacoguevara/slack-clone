8c4e17051f39e6511eb9a96af0549e63726d02c8
Paco Guevara <pacoguevaraa@gmail.com>
2020-08-21 16:49:05 -0700

    add sidebar header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     // BEM naming convention
     <div className="App">
       <Header />
-      {/* Sidebar */}
+      <div className="app__body">
+        <Sidebar />
+      </div>
       {/* react touter -> chat screen */}
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.css";
-import { Avatar, Input, Icon } from "@material-ui/core";
+import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import SearchIcon from "@material-ui/icons/Search";
 import HelpIcon from "@material-ui/icons/Help";
