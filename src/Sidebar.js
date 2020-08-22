00384430c55437a23305cedb3806edcd6b9eac07
Paco Guevara <pacoguevaraa@gmail.com>
2020-08-21 19:48:59 -0700

    add sidebar options

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,15 @@ import React from "react";
 import "./Sidebar.css";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessCopyIcon from "@material-ui/icons/ExpandLess";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
   return (
@@ -16,6 +25,15 @@ function Sidebar() {
         </div>
         <CreateIcon />
       </div>
+      <SidebarOption Icon={InsertCommentIcon} title="Thred" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="Files browser" />
+      <SidebarOption Icon={ExpandLessCopyIcon} title="Show less" />
+      <SidebarOption title="Youtube" />
     </div>
   );
 }
